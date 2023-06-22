1fcaba04813cc700a0beee51e1092817e6893283
Fernando Pérez <ferperezm95@gmail.com>
2023-06-22 08:51:03 -0600

    Adding section 5

---
+++ 61-75/data.js
@@ -0,0 +1,4 @@
+// Verbos en pasado, pasado participio y significado
+const past = ["Meant", "Met", "Paid", "Put", "Quit", "Read", "Rode", "Rang", "Rose", "Ran", "Said", "Saw", "Sold", "Sent", "Set"];
+const pastParticiple = ["Meant", "Met", "Paid", "Put", "Quit", "Read", "Ridden", "Rung", "Risen", "Run", "Said", "Seen", "Sold", "Sent", "Set"];
+const meaning = ["Querer decir", "Conocer", "Pagar", "Poner", "Abandonar", "Leer", "Montar, ir", "Llamar por teléfono", "Elevar", "Correr", "Decir", "Ver", "Vender", "Enviar", "Fijar"];
