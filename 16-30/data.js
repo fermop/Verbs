be7fd8cbbf9833a3a66977a12e776825d44b631a
Fernando Pérez <ferperezm95@gmail.com>
2023-06-21 22:45:50 -0600

    Adding section 2

---
+++ 16-30/data.js
@@ -0,0 +1,4 @@
+// Verbos en pasado, pasado participio y significado
+const past = ["Came", "Cost", "Crept", "Cut", "Dealt", "Did", "Drew", "Dreamt", "Drank", "Drove", "Ate", "Fell", "Fed", "Felt", "Fought"];
+const pastParticiple = ["Come", "Cost", "Crept", "Cut", "Dealt", "Done", "Drawn", "Dreamt", "Drunk", "Driven", "Eaten", "Fallen", "Fed", "Felt", "Fought"];
+const meaning = ["Venir", "Costar", "Arrastrarse", "Cortar", "Negociar", "Hacer", "Dibujar", "Soñar", "Beber", "Conducir", "Comer", "Caer", "Alimentar", "Sentir", "Pelear"];
