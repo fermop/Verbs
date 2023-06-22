ee109d37453dffd914bbc8f7b4846f8c6ddaa631
Fernando Pérez <ferperezm95@gmail.com>
2023-06-21 21:37:00 -0600

    Adding section 7 and DRYing code on js

---
+++ 91-105/data.js
@@ -0,0 +1,4 @@
+// Verbos en pasado, pasado participio y significado
+const past = ["Spent", "Spilt", "Split", "Spoilt", "Spread", "Stood", "Stole", "Stung", "Stank", "Struck", "Swore", "Swept", "Swam", "Took", "Taught"];
+const pastParticiple = ["Spent", "Spilt", "Split", "Spoilt", "Spread", "Stood", "Stolen", "Stung", "Stunk", "Struck", "Sworn", "Swept", "Swum", "Taken", "Taught"];
+const meaning = ["Gastar", "Derramar", "Dividir", "Estropear", "Extenderse", "Estar de pie", "Robar", "Picar", "Apestar", "Golpear", "Jurar", "Trapear", "Nadar", "Tomar", "Enseñar"];
