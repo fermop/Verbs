0a4f144a8e7ddcfdacddb05660eb3f2e28825334
Fernando Pérez <ferperezm95@gmail.com>
2023-06-22 08:26:59 -0600

    Adding section 4 and closing div tag

---
+++ 46-60/data.js
@@ -0,0 +1,4 @@
+// Verbos en pasado, pasado participio y significado
+const past = ["Hid", "Hit", "Held", "Hurt", "Kept", "Knelt", "Knew", "Led", "Learnt", "Left", "Lent", "Let", "Lay", "Lost", "Made"];
+const pastParticiple = ["Hidden", "Hit", "Held", "Hurt", "Kept", "Knelt", "Known", "Led", "Learnt", "Left", "Lent", "Let", "Lain", "Lost", "Made"];
+const meaning = ["Esconderse", "Golpear", "Tener, mantener", "Herir", "Guardar", "Arrodillarse", "Saber", "Liderar", "Aprender", "Dejar", "Prestar", "Permitir", "Yacer", "Perder", "Hacer"];
