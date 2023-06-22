c6b5a63213c112e2e4900f2cfba499b344c5b886
Fernando Pérez <ferperezm95@gmail.com>
2023-06-22 07:25:57 -0600

    add section 3

---
+++ 31-45/data.js
@@ -0,0 +1,4 @@
+// Verbos en pasado, pasado participio y significado
+const past = ["Found", "Fled", "Flew", "Forgot", "Forgave", "Forsook", "Froze", "Got", "Gave", "Went", "Ground", "Grew", "Hung", "Had", "Heard"];
+const pastParticiple = ["Found", "Fled", "Flown", "Forgotten", "Forgiven", "Forsaken", "Frozen", "Got", "Given", "Gone", "Ground", "Grown", "Hung", "Had", "Heard"];
+const meaning = ["Encontrar", "Huir", "Volar", "Olvidar", "Perdonar", "Abandonar", "Congelar", "Tener, obtener", "Dar", "Ir", "Moler", "Crecer", "Colgar", "Tener", "Oír"];
