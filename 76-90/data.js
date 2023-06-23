24ac9db1da2f1524fd1274551a24aecbbdf8237b
Fernando Pérez <ferperezm95@gmail.com>
2023-06-23 07:38:10 -0600

    Fixing meaning of verb 81

--- 76-90/data.js
+++ 76-90/data.js
@@ -1,4 +1,4 @@
 // Verbos en pasado, pasado participio y significado
 const past = ["Sewed", "Shook", "Shone", "Shot", "Showed", "Shrank", "Shut", "Sang", "Sank", "Sat", "Slept", "Slid", "Sowed", "Spoke", "Spelt/spelled"];
 const pastParticiple = ["Sewn", "Shaken", "Shone", "Shot", "Shown", "Shrunk", "Shut", "Sung", "Sunk", "Sat", "Slept", "Slid", "Sown", "Spoken", "Spelt/spelled"];
-const meaning = ["Coser", "Sacudir", "Brillar", "Disparar", "Mostrar", "Escoger", "Cerrar", "Cantar", "Hundir", "Sentarse", "Dormir", "Deslizar", "Sembrar", "Hablar", "Deletrear"];
+const meaning = ["Coser", "Sacudir", "Brillar", "Disparar", "Mostrar", "Encoger", "Cerrar", "Cantar", "Hundir", "Sentarse", "Dormir", "Deslizar", "Sembrar", "Hablar", "Deletrear"];
