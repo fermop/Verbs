b8a88a88468518cbb9586c3d54dbd2b8f74b485a
Fernando Pérez <ferperezm95@gmail.com>
2023-07-08 14:58:13 -0600

    fix spelling mistake of verb number 111

--- 105-118/data.js
+++ 105-118/data.js
@@ -1,4 +1,4 @@
 // Verbos en pasado, pasado participio y significado
 const past = ["Tore", "Told", "Thought", "Threw", "Trode", "Understood", "Woke", "Wore", "Wove", "Wept", "Won", "Wrung", "Wrote"];
-const pastParticiple = ["Torn", "Told", "Thought", "Thrown", "Trodden/trod", "Undertood", "Woken", "Worn", "Woven", "Wept", "Won", "Wrung", "Written"];
+const pastParticiple = ["Torn", "Told", "Thought", "Thrown", "Trodden/trod", "Understood", "Woken", "Worn", "Woven", "Wept", "Won", "Wrung", "Written"];
 const meaning = ["Rasgar", "Decir", "Pensar", "Lanzar", "Pisotear", "Entender", "Despertarse", "Llevar puesto", "Tejer", "Llorar", "Ganar", "Retorcer", "Escribir"];
